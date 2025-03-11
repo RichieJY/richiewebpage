98675adbd8d372090301118255819278feb4f47e
Richie Y <ryoder@una.edu>
2025-03-11 10:53:10 -0500

    Delete scripts directory

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    console.log('Page Loaded Successfully!');
-    
-    // Prevent navbar from collapsing when clicking links
-    $('.navbar-nav a').click(function(){
-        $('.navbar-collapse').collapse('hide');
-    });
-});
